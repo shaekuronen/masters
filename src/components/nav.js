68cc7a4bf3005865be6d860e059c25ae199329f0
Shae Kuronen <shae.kuronen@gmail.com>
2015-06-25 10:21:55 -0700

    Thu Jun 25 10:21:55 PDT 2015

---
+++ src/components/nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import localLinks from 'local-links'
+
+export default React.createClass({
+  onClick (event) {
+    const pathname = localLinks.getLocalPathname(event)
+    if (pathname) {
+      event.preventDefault()
+      app.router.history.navigate(pathname)
+    }
+  },
+  render () {
+    return (
+      <nav className='top-nav top-nav-light cf' role='navigation' onClick={this.onClick}>
+        <input id='menu-toggle' className='menu-toggle' type='checkbox'/>
+        <label htmlFor='menu-toggle'>Menu</label>
+        <ul className='list-unstyled list-inline cf'>
+          <li>Labelr</li>
+          <li><a href='http://google.com'>Google</a></li>
+          <li><a href='/test'>Test</a></li>
+          <li><a href='/repos'>Repos</a></li>
+          <li className='pull-right'><a href='/logout'>Logout</a></li>
+        </ul>
+      </nav>
+    )
+  }
+})
